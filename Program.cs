469c555525d8309aedf1bdc95f9e02d34ddfeec1
Ruan <ruangustavo50711@gmail.com>
2020-11-17 16:33:32 -0300

    Tabuada Finalizada

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C__While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja saber a tabuada de qual numero?: ");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+            for (int contador = 10; contador > 0; contador--)
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
